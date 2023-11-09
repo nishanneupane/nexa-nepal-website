9821b1617f9fd340548ff7b32e8eaa85ae8392af
unknown <nishanneupane368@gmail.com>
2023-11-10 00:46:29 +0545

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Nexa Nepal',
-  description: 'It is an digital marketing agency.',
+  description: 'It is an digital marketing agency based in nepal.',
 }
 
 export default function RootLayout({
